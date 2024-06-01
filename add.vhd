c428ab56b66e5fa72febf64984e54f0d00022442
AhmadReshad1 <133067691+AhmadReshad1@users.noreply.github.com>
2024-06-01 14:29:46 -0400

    Add files via upload

---
+++ add.vhd
@@ -0,0 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+
+entity add is
+port(
+    A   	 :    in  std_logic_vector(31 downto 0);
+    B   	 :    out std_logic_vector(31 downto 0) 
+	);
+end add;
+
+architecture behavior of add is
+begin
+B <= A + 1;
+end Behavior;
+    	
+
+
+		
